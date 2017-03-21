e67b4591d868ccc77d24fa498453ed64e793aeae
Terry O'Toole <otooleterrence@gmail.com>
2017-03-21 11:57:49 -0400

    Utils file updated

---
+++ app/utils/server.settings.js
@@ -0,0 +1,5 @@
+// When running locally, change which line is commented
+//this should probably be set with a dev/production environment variable
+
+export const serverLocation = 'http://pair-server.herokuapp.com';
+// export const serverLocation = 'http://localhost:1337';
