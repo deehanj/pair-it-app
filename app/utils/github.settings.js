071191965905fa969216d69fc577f47415008b83
Terry O'Toole <otooleterrence@gmail.com>
2017-03-21 12:04:55 -0400

    Delete github.settings.js

--- app/utils/github.settings.js
+++
@@ -1,6 +0,0 @@
-//this is how we identify with github's api.
-//this file is ignored by git, should not be on github
-
-export const GITHUB_CLIENT_ID = "ec66fd9119c4b4195573";
-export const GITHUB_CLIENT_SECRET = "16f743fd7cea901596d43d88fb87e01f0e5281d6";
-export const GITHUB_CLIENT_SCOPES = ["user", "repo", "read:org"];
