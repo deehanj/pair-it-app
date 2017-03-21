289d49a460e3ae9748b1475f272df55198f9d2b3
Terry O'Toole <otooleterrence@gmail.com>
2017-03-21 16:46:56 -0400

    oauth verified on git, token received, not rerouting to home

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,12 +2,14 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import files from './FilesReducer'
+import auth from './auth'
 import VideoChatReducer from '../VideoChat/VideoChatReducer';
 
 const rootReducer = combineReducers({
   routing,
   fileSystem: files,
-  VideoChat: VideoChatReducer
+  VideoChat: VideoChatReducer,
+  auth
 });
 
 export default rootReducer;
--- app/utils/github.settings.js
+++ app/utils/github.settings.js
@@ -1,6 +1,8 @@
 //this is how we identify with github's api.
 //this file is ignored by git, should not be on github
 
-export const GITHUB_CLIENT_ID = "ec66fd9119c4b4195573";
-export const GITHUB_CLIENT_SECRET = "16f743fd7cea901596d43d88fb87e01f0e5281d6";
-export const GITHUB_CLIENT_SCOPES = ["user", "repo", "read:org"];
+export default {
+  client_id: "ec66fd9119c4b4195573",
+  secret: "16f743fd7cea901596d43d88fb87e01f0e5281d6",
+  scopes: ["user", "repo", "read:org"],
+}
