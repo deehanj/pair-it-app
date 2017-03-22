f632c40af2dc85a320293f59f9c3219a5d39316a
Terry O'Toole <otooleterrence@gmail.com>
2017-03-22 12:39:19 -0400

    moved login actions to reducer

--- app/reducers/auth.js
+++ app/reducers/auth.js
@@ -1,4 +1,20 @@
-import { LOGIN, LOGOUT } from '../actions';
+// import { LOGIN, LOGOUT } from '../actions';
+import { apiRequest, apiRequestAuth } from '../utils/api-requests';
+import { fetchUsername } from '../reducers/user';
+import options from '../utils/github.settings';
+
+export function makeAsyncActionSet(actionName) {
+  return {
+    REQUEST: actionName + '_REQUEST',
+    SUCCESS: actionName + '_SUCCESS',
+    FAILURE: actionName + '_FAILURE'
+  };
+}
+
+
+export const LOGIN = makeAsyncActionSet('LOGIN');
+export const LOGOUT = 'LOGOUT';
+
 
 const initialState = {
   response: {},
@@ -6,6 +22,7 @@ const initialState = {
   isFetching: false,
   failed: false
 };
+
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
@@ -39,4 +56,37 @@ export default function reducer(state = initialState, action) {
     default:
       return state;
   }
-};
+}
+
+
+export function loginUser(code) {
+  return (dispatch, getState) => {
+
+    const url = 'https://github.com/login/oauth/access_token';
+    const method = 'POST';
+    const data = {
+      client_id: options.client_id,
+      client_secret: options.secret,
+      code: code
+    };
+
+    dispatch({type: LOGIN.REQUEST});
+
+    return apiRequest(url, method, data)
+      .then(function (response) {
+        return dispatch({type: LOGIN.SUCCESS, payload: response.data});
+      })
+      .then(() => {
+        fetchUsername()();
+      })
+      .catch(function (error) {
+        console.log(error);
+        dispatch({type: LOGIN.FAILURE, payload: error.response.data});
+      });
+
+  };
+}
+
+export function logout() {
+  return { type: LOGOUT };
+}
